951af98db6a329aa209f22b42629780de5932a72
brijamfitz <brianjfitzgerald@gmail.com>
2019-01-29 18:12:27 -0500

    Updates

---
+++ precache-manifest.52e4f0e16ed999d079c0f33bac731e83.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6f491c87c71a0fe2d59d",
+    "url": "/brian-fitzgerald/static/js/runtime~main.6f491c87.js"
+  },
+  {
+    "revision": "5778ab6de7f1d4558a39",
+    "url": "/brian-fitzgerald/static/js/main.5778ab6d.chunk.js"
+  },
+  {
+    "revision": "52773461455c439ef26c",
+    "url": "/brian-fitzgerald/static/js/1.52773461.chunk.js"
+  },
+  {
+    "revision": "5778ab6de7f1d4558a39",
+    "url": "/brian-fitzgerald/static/css/main.be43448b.chunk.css"
+  },
+  {
+    "revision": "add72025a2770d3609bd65d408b13c77",
+    "url": "/brian-fitzgerald/index.html"
+  }
+];
