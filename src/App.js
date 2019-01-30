360e032641d936dfb16ae6e67b89084e8908da40
brijamfitz <brianjfitzgerald@gmail.com>
2019-01-30 11:03:13 -0500

    npm install

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ const App = () => {
     <Router>
       <div>
         <Switch>
-          <Route exact path="/brian-fitzgerald" component={Home} />
-          <Route exact path="/brian-fitzgerald/projects" component={Projects} />
+          <Route exact path={process.env.PUBLIC_URL + "/"} component={Home} />
+          <Route exact path={process.env.PUBLIC_URL + "/projects"} component={Projects} />
         </Switch>
       </div>
     </Router>
